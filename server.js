8a560b8bb68a2476ae1ac3df632b2146718f35e8
Nichole <nicholemcgrew@gmail.com>
2023-10-16 15:19:29 -0500

    created mongoDB connection

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors');
+const exp = require('constants');
+require('dotenv').config();
+
+//may see bodyParser, but was replaced by middleware
+//const bodyParser = require('body-parser);
+
+const app = express()
+
+//Middleware
+//translate data into a readable format
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(cors())
+app.use(express.static('public'))
+
+//Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', ()=> {
+    console.log("Connected to MongoDb")
+})
