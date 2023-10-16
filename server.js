f30ec3d064f2a0679d0ed85179f7ad93e0422d53
Nichole <nicholemcgrew@gmail.com>
2023-10-16 15:24:49 -0500

    updated gitignore

--- server.js
+++ server.js
@@ -24,3 +24,7 @@ db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', ()=> {
     console.log("Connected to MongoDb")
 })
+
+//Start server
+const PORT = 4000
+app.listen()
